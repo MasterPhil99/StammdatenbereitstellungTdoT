38291091048aa92a089c379f22e5499cbcbcfec0
Farag42 <32298872+Farag42@users.noreply.github.com>
2018-01-16 16:26:03 +0100

    Sprint 3 UserStories

--- server.js
+++ server.js
@@ -87,19 +87,14 @@ app.use('/login', login);
 app.use(['/messages', '/message'], msg);
 
 app.use(function (req, res, next) {
-    if (req.method === 'OPTIONS') {
-        console.log('!OPTIONS');
-        var headers = {};
-        // IE8 does not allow domains to be specified, just the *
-        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-        headers["Access-Control-Allow-Origin"] = "*";
-        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-        headers["Access-Control-Allow-Credentials"] = false;
-        headers["Access-Control-Max-Age"] = '90000'; // 24 hours
-        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-        res.writeHead(200, headers);
-        res.end();
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+    } else {
+        res.header('Access-Control-Allow-Origin', '*');
     }
+    res.header('Access-Control-Allow-Methods', 'GET,PUT, POST,DELETE');
+
+    next();
 });
 
 // catch 404 and forward to error handler
