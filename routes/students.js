35dcbc1e31a61e85cc1a38dadccfaa4e2b0796c8
Farag42 <32298872+Farag42@users.noreply.github.com>
2018-01-20 12:53:40 +0100

    Various Bug Fixes

--- routes/students.js
+++ routes/students.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongo = require('mongodb');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     //res.send('list with all students');
     //needs consideration whether everyone should be able to get a list of students or not
     var pLastName = req.query.lastname;
@@ -13,12 +13,6 @@ router.get('/', function(req, res, next) {
     var cFlag = false;
     var pUserName = req.query.username;
     var unFlag = false;
-    var pTeacher = req.query.teacher;
-    var tFlag = false;
-    var pStand = req.query.stand;
-    var sFlag = false;
-    var pAssigned = req.query.assigned;
-    var asFlag = false;
     var query = { category: 'student' };
 
     if (typeof pLastName != undefined && pLastName != null) {
@@ -41,19 +35,7 @@ router.get('/', function(req, res, next) {
         query.username = { $regex: ".*" + pUserName + ".*" }
     }
 
-    if (typeof pTeacher != undefined && pTeacher != null) {
-        tFlag = true;
-    }
-
-    if (typeof pStand != undefined && pStand != null) {
-        sFlag = true;
-    }
-
-    if (typeof pAssigned != undefined && pAssigned != null) {
-        asFlag = true;
-    }
-
-    if (lnFlag || fnFlag || cFlag || unFlag || tFlag || sFlag) {
+    if (lnFlag || fnFlag || cFlag || unFlag) {
         req.db.collection('users').find(query, { /*password: 0,*/ category: 0 }).toArray(function (err, results) {
             if (typeof results == undefined || results.length <= 0) {
                 res.status(404);
@@ -61,43 +43,6 @@ router.get('/', function(req, res, next) {
             } else {
                 var resultsToReturn = [];
                 for (var item in results) {
-                    //console.log(results[item]);
-                    /*req.db.collection('stands').find({ students: [results[item]._id] }).toArray(function (err, doc) {
-                        results[item].id = results[item]._id;
-                        results[item].link = req.baseURL + "/student/" + results[item].id;
-
-                        if (typeof doc != undefined && doc.length > 0) {
-                            results[item].assigned = true;
-
-                            if (tFlag) {
-                                results[item].teacher = doc.assigned; //get the entire teacher?
-                            }
-
-                            if (sFlag) {
-                                results[item].stand = doc;
-                            }
-                        } else {
-                            results[item].assigned = false;
-                        }
-
-                        if (asFlag) {
-                            if (pAssigned) {
-                                if (results[item].assigned) {
-                                    resultsToReturn.push(results[item]);
-                                }
-                            } else {
-                                if (!results[item].assigned) {
-                                    resultsToReturn.push(results[item]);
-                                }
-                            }
-                        } else {
-                            resultsToReturn.push(results[item]);
-                        }
-
-                        res.send(resultsToReturn); //there needs to be a loop in here but ?????
-                        //idk how to get all of the students
-                    });*/
-
                     results[item].id = results[item]._id;
                     results[item].link = req.baseURL + "/students/" + results[item].id;
                 }
@@ -106,10 +51,10 @@ router.get('/', function(req, res, next) {
         });
     }
     else {
-        req.db.collection('users').find({ category: 'student' }, { category: 0 }).toArray(function (err, results) {
+        req.db.collection('users').find({ category: 'student' }, { _id: 1, class: 1, lastname: 1 }).toArray(function (err, results) {
             if (typeof results == undefined || results.length <= 0) {
                 res.status(404);
-                res.send('No students exist!');
+                res.send('Student not found!');
             } else {
                 for (var item in results) {
                     results[item].id = results[item]._id;
@@ -135,7 +80,7 @@ router.get('/:id', function (req, res, next) {
                     if (typeof doc != undefined && doc.length > 0) {
                         doc[0].id = doc[0]._id;
                         results[0].assigned = true;
-                        results[0].teacher = doc[0].assigned; //get the entire teacher?
+                        results[0].teacher = doc[0].assigned;
                         results[0].stand = doc[0];
                     }
 
@@ -153,7 +98,30 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.get('/:id/stand',function(req, res, next) {
-    res.send('list of stands with this user');
+    //res.send('list of stands with this user');
+    try {
+        var id = new mongo.ObjectID(req.params.id);
+
+        req.db.collection('users').find({ _id: id, category: 'student' }, { /*password: 0,*/ category: 0 }).toArray(function (err, results) {
+            if (typeof results != undefined && results.length > 0 && results[0] != undefined) {
+                req.db.collection('stands').find({ students: results[0]._id }).toArray(function (err, doc) {
+                    console.log(doc);
+                    if (typeof doc != undefined && doc.length > 0) {
+                        res.send(doc);
+                    } else {
+                        res.status(404);
+                        res.send("This student is in no stand!");
+                    }
+                });
+            } else {
+                res.status(404);
+                res.send("Student not found!");
+            }
+        });
+    } catch (err) {
+        res.status(400);
+        res.send("Invalid ID! " + err.message);
+    }
 });
 
 module.exports = router;
